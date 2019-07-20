90ba7ce14ff361bebc105d6bb2bf99a0a59c962f
ant9112 <51388219+ant9112@users.noreply.github.com>
2019-07-20 18:57:27 -0500

    Update TestLogin.java

--- src/test/java/pruebas/TestLogin.java
+++ src/test/java/pruebas/TestLogin.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
- * @author Andrea en la rama Desarrollo
+ * @author Andrea
  *
  */
 public class TestLogin {
